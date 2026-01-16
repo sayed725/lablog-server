baab93bacf4676a8085b8fcb11178635c8793868
sayed725 <ssayed72533@gmail.com>
2026-01-16 21:37:42 +0600

    added equipment route

---
+++ src/modules/equipment/equipment.controller.ts
@@ -0,0 +1,30 @@
+import { RequestHandler } from "express";
+import { prisma } from "../../lib/prisma";
+
+const createEquipment: RequestHandler = async (req, res) => {
+  try {
+    const payload = req.body;
+
+    const equipment = await prisma.equipment.create({
+      data: payload,
+    });
+
+    res.send({ message: "Equipment Added", data: equipment });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getEquipments: RequestHandler = async (req, res) => {
+  try {
+    const data = await prisma.equipment.findMany();
+    res.send({ message: "Equipments", data });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export const equipmentController = {
+  createEquipment,
+  getEquipments,
+};
---
+++ src/modules/equipment/equipment.route.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { equipmentController } from "./equipment.controller";
+// import auth from "../../middleware/auth";
+
+const equipmentRouter = Router();
+
+equipmentRouter.post(
+  "/",
+//   auth("equipment", "create"),
+  equipmentController.createEquipment
+);
+equipmentRouter.get(
+  "/",
+//   auth("equipment", "read"),
+  equipmentController.getEquipments
+);
+
+export default equipmentRouter;
