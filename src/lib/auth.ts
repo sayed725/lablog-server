19eab2a702db4fab6235f3cdcf962d5174a27b0b
sayed725 <ssayed72533@gmail.com>
2026-01-17 10:58:02 +0600

    added two factor otp auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,16 @@
+
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import { PrismaClient } from "../generated/prisma/client";
 import { prisma } from "./prisma";
+import { twoFactor } from "better-auth/plugins";
 // If your Prisma file is located elsewhere, you can change the path
+import { Resend } from 'resend';
 
-
+const resend = new Resend('re_NnmhaRfH_QGNHCu4P6SB2Qn1oKSDeTKte');
 
 export const auth = betterAuth({
+    appName: "LabLog",
     database: prismaAdapter(prisma, {
         provider: "postgresql", // or "mysql", "postgresql", ...etc
     }),
@@ -24,7 +28,25 @@ export const auth = betterAuth({
             clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
             redirectURI: `${process.env.FRONTEND_URL}/api/auth/callback/github`,
         }
-    }
+    },
+
+    plugins: [
+        twoFactor({
+      otpOptions: {
+        period: 2,
+        async sendOTP({ user, otp }, ctx) {
+        //   console.log({ user, otp });
+
+          await resend.emails.send({
+            from: "LagLog <onboarding@resend.dev>",
+            to: user.email,
+            subject: "Two factor authentication",
+            html: `<p>Your OTP is <b>${otp}</b></p>`,
+          });
+        },
+      },
+    }),
+    ]
 
     
 });
